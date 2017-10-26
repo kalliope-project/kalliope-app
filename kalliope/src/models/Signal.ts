1ecb8b740d0f8011ec2625863f1bf8eef5b3ca33
ThiBuff <buffet.thibaud@gmail.com>
2017-10-26 22:49:34 +0200

    [Feature] #8 Geolocation with Signal abstract

--- kalliope/src/models/Signal.ts
+++ kalliope/src/models/Signal.ts
@@ -1,29 +1,28 @@
-import {Param} from "./param";
+import {Param} from "./Param";
 
 /**
  * The model class corresponding to the Signal
  * @class Signal
  */
 
-export interface SignalConstructor {
-    new (name: string, params: Array<Param>): Signal
-}
-
 export abstract class Signal {
-
-    protected name: string;
-    protected params: Array<Param>;
-
-    abstract toString(): string;
 
     /**
      * @constructor
      * @param value {string} the order value
      * @param params {Array<Param>} the list of Param
      */
-    constructor(name: string, params: Array<Param>) {
+    constructor(public name: string, protected params: Array<Param>) {
         this.name = name;
         this.params = params;
+    }
+
+    /**
+     * Convert a Signal to String
+     * @return {string} The string corresponding to the order
+     */
+    toString(): string {
+        return "Name: "+this.name+", params -> "+this.params;
     }
 
 }
