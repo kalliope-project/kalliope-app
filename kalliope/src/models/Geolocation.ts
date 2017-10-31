80d6e63f081d9c28f62c3fbb256b356d28b3c93e
ThiBuff <buffet.thibaud@gmail.com>
2017-11-01 00:18:42 +0100

    [Feature] #8 geolocation

--- kalliope/src/models/Geolocation.ts
+++ kalliope/src/models/Geolocation.ts
@@ -18,24 +18,13 @@ export class Geolocation extends Signal {
     public name: string;
     public params: Array<Param>;
 
-    private geofence: Geofence = new Geofence();
 
     /**
      * @constructor
-     * @param name {string} the geolocation name
      * @param params {Array<Param>} the list of Param
      */
-    constructor(name: string, params: Array<Param>) {
+    constructor(name:string, params: Array<Param>) {
         super(name, params);
-        // initialize the geofence
-        this.geofence.initialize().then(
-            // resolved promise does not return a value
-            () => console.log('[Geolocation] Geofence Plugin Ready'),
-            (err) => console.log(err)
-        );
-        this.addGeofence();
-        this.geofence.onTransitionReceived().forEach(geo => console.log('[] Geofence transition detected :' + this.name)
-        );
     }
 
     public _getLatitude(): number {
@@ -48,21 +37,6 @@ export class Geolocation extends Signal {
 
     public _getRadius(): any {
         return this.params.find(p => p.name == 'radius').value;
-    }
-
-    private addGeofence() {
-        let fence = {
-            id: this.name, //any unique ID
-            latitude: this._getLatitude(), //center of geofence radius
-            longitude: this._getLongitude(),
-            radius: this._getRadius(), //radius to edge of geofence in meters
-            transitionType: 1 // TransitionType.ENTER
-        }
-
-        this.geofence.addOrUpdate(fence).then(
-            () => console.log('[Geolocation] Geofence ' + this.name + ' added'),
-            (err) => console.log('[Geolocation] Geofence ' + this.name + ' failed to add')
-        );
     }
 }
 
--- kalliope/src/pages/synapses/synapses.component.ts
+++ kalliope/src/pages/synapses/synapses.component.ts
@@ -4,6 +4,8 @@ import {SynapsesService} from './synapses.service';
 import {SettingsService} from './../settings/settings.service';
 import {Settings} from './../settings/settings';
 import {Synapse} from "../../models/Synapse";
+import {Geofence} from "@ionic-native/geofence";
+import {Geolocation} from "../../models/Geolocation";
 
 /**
  * UI Component and Behaviour for the Synapse page
@@ -15,7 +17,8 @@ import {Synapse} from "../../models/Synapse";
 })
 export class SynapsesPage {
 
-    synapses: Array<Synapse> = [];
+    synapsesToDisplay: Array<Synapse> = [];
+    geofence: Geofence = new Geofence();
     settings: Settings;
 
     /**
@@ -27,34 +30,72 @@ export class SynapsesPage {
     constructor(public navCtrl: NavController,
                 private synapseService: SynapsesService,
                 public settingsService: SettingsService) {
-
         this.settings = settingsService.getDefaultSettings();
+        if (this.settings.geolocation) {
+            // initialize the geofence
+            this.geofence.initialize().then(
+                // resolved promise does not return a value
+                () => console.log('[Geolocation] Geofence Plugin Ready'),
+                (err) => console.log(err)
+            );
+        }
     }
 
     ngOnInit() {
-        this.getOrderSynapses();
+        this.getSynapsesToDisplay();
+        // if (this.settings.geolocation) {
+        //     //this.initGeolocationSynapses();
+        // }
     }
 
     /**
      * Retrieve the list of sysnapse from the Kalliope Core API
      */
-    getOrderSynapses() {
+    getSynapsesToDisplay() {
         return this.synapseService.getSynapses(this.settings).subscribe(
             response => {
-                let reponseOrder = response.filter(synapse => synapse.signal.name == 'order');
-                console.log("[SynapsesPage] getOrderSynapses: fetched synapses list -> " + JSON.stringify(reponseOrder));
-                this.synapses = reponseOrder;
+                let synapsesToDisplay = response.filter(syn => SynapsesPage.selectSynapseToDisplay(this.settings, syn));
+                console.log("[SynapsesPage] getSynapsesToDisplay: fetched synapses list -> " + JSON.stringify(synapsesToDisplay));
+                this.synapsesToDisplay = synapsesToDisplay;
             },
             err => {
-                console.log("[SynapsesPage] getOrderSynapses: Error fetching the synapses list ! -> " + err);
-                this.synapses = [];
+                console.log("[SynapsesPage] getSynapsesToDisplay: Error fetching the synapses list ! -> " + err);
+                this.synapsesToDisplay = [];
             }
         );
     }
 
-    /*
-    * TODO select only Order synapses
-    * */
+    /*TODO manage geolocation properly */
+    private static selectSynapseToDisplay(settings: Settings, synapse: Synapse)  {
+        if (settings.geolocation) {
+            return synapse.signal.name == 'order' || synapse.signal.name == 'geolocation';
+        }
+        return synapse.signal.name == 'order'
+    }
+
+    private initGeolocationSynapses() {
+        this.synapsesToDisplay.filter(syn => syn.signal.name == 'geolocation').forEach(this.initGeolocationTrigger)
+    }
+
+    private initGeolocationTrigger(geolocationSynapse: Synapse) {
+        // casting signal to geolocation
+        let geolocation: Geolocation = geolocationSynapse.signal as Geolocation;
+        let fence = {
+            id: geolocation.name, //any unique ID
+            latitude: geolocation._getLatitude(), //center of geofence radius
+            longitude: geolocation._getLongitude(),
+            radius: geolocation._getRadius(), //radius to edge of geofence in meters
+            transitionType: 1 // TransitionType.ENTER
+        }
+
+        this.geofence.addOrUpdate(fence).then(
+            () => console.log('[Geolocation] Geofence ' + geolocation.name + ' added'),
+            (err) => console.log('[Geolocation] Geofence ' + geolocation.name + ' failed to add')
+        );
+
+        this.geofence.onTransitionReceived().forEach(geo => console.log('Geofence transition detected :' + geo.toString()));
+    }
+
 
     /**
      * Run a synapse calling the Kalliope Core API
@@ -67,6 +108,8 @@ export class SynapsesPage {
                     console.log("[SynapsesPage] runSynapse: Response from running synapse -> " + JSON.stringify(response));
                 })
     }
+
+
 }
 
 
