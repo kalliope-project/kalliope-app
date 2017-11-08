e5283283d56b4cc471a110509c1daf0b7fbf947d
ThiBuff <buffet.thibaud@gmail.com>
2017-11-08 22:30:34 +0100

    [Feature] #8 add raise geolocation and start chat page.

--- kalliope/src/pages/synapses/synapses.service.ts
+++ kalliope/src/pages/synapses/synapses.service.ts
@@ -6,6 +6,7 @@ import {Settings} from "../settings/settings";
 import {Synapse} from "../../models/Synapse";
 import {Observable} from "rxjs/Observable";
 import {Serialization} from "../../serialization/Serialization";
+import {OrderResponse} from "../../models/orderResponse";
 
 /**
  * Service to manage the Synapse operations using the Kalliope Core API
@@ -40,33 +41,21 @@ export class SynapsesService {
      * Run a synapse from using the Kalliope Core API (/synapses/start/id/)
      * @param synapse {Synapse} the synapse to run
      * @param settings {Settings} the settings to access the Kalliope Core API
-     * @return {Observable<array<Synapse>>}
+     * @return {Observable<OrderResponse>}
      */
     runSynapse(synapse: Synapse,
-               settings: Settings): Observable<Array<Synapse>> {
-        let headers = new Headers();
-        headers.append('Authorization', 'Basic ' + btoa(settings.username + ':' + settings.password));
-        const options = new RequestOptions({headers: headers});
-
-        // TODO kalliope Core v0.4.4 -> API does not handle Param POST !!
-        // let param_dict = {}
-        // for (let param of synapse.signal.params) {
-        //     param_dict[param.name] = param.value;
-        // }
-        // let body = JSON.stringify(param_dict); // Stringify payload
-
-        return this.http.post('http://' + settings.url + '/synapses/start/id/' + synapse.name, undefined, options)
-            .map(res => res.json())
+               settings: Settings): Observable<OrderResponse> {
+        return this.runSynapseByName(synapse.name, settings);
     }
 
     /**
      * Run a synapse by name from using the Kalliope Core API (/synapses/start/id/)
      * @param synapseName {string} the synapse name to run
      * @param settings {Settings} the settings to access the Kalliope Core API
-     * @return {Observable<array<Synapse>>}
+     * @return {Observable<OrderResponse>}
      */
     runSynapseByName(synapseName: string,
-               settings: Settings): Observable<Array<Synapse>> {
+               settings: Settings): Observable<OrderResponse> {
         let headers = new Headers();
         headers.append('Authorization', 'Basic ' + btoa(settings.username + ':' + settings.password));
         const options = new RequestOptions({headers: headers});
@@ -78,6 +67,6 @@ export class SynapsesService {
         // let body = JSON.stringify(param_dict); // Stringify payload
 
         return this.http.post('http://' + settings.url + '/synapses/start/id/' + synapseName, undefined, options)
-            .map(res => res.json())
+            .map(res => OrderResponse.responseToObject(res.json()))
     }
 }
